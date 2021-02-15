3a112916f2945cc201cd72f639e0af0a75605a7f
yitd <55494975+yitd@users.noreply.github.com>
2021-02-16 05:39:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-//$anyip值为1发送服务器IP头，值为2则发送随机IP，值为3发送客户端IP
-$anyip = 1;
 $host = $_SERVER['HTTP_HOST'];
 $path = $_SERVER['REQUEST_URI'];
 $https = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
+//$anyip值为1发送服务器IP头，值为2则发送随机IP，值为3发送客户端IP，仅在部分网站中有效
+$anyip = 1;
 if (substr($path, -2) == "*q") {
     del_cookie();
     header("Location: " . $https . $host);
